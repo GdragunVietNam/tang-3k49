b07eb35d051dd4e7f661438ec389a770b249188e
GdragunVietNam <92350310+GdragunVietNam@users.noreply.github.com>
2021-10-18 10:09:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Cac cau ak`!',
+    introTitle: 'Các cậu à :3 !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cau cứ đi nhầm đường
     Lạc vào tim to' lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'NGAY PHU LU VIET NAM😙',
-    desc: 'VI DICH NEN BON TO CHUA TO CHUC DC CHAP NHAN K NE ',
-    btnYes: 'Chap nhan <33',
-    btnNo: 'Không never :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cac cau lai chon bon to <3',
-    btnReply: 'Gửi cho to <3',
-    reply: 'qua na tuyet voi <33333333',
+    title: 'Ngày Phụ Nữ Việt Nam 20/10😙',
+    desc: 'Mong tất cả những món quà nho nhỏ vừa rồi làm các cậu có những phút giây vui vẻ  ',
+    btnYes: 'Vui chứ <33',
+    btnNo: 'Không vui :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao các cậu lại chọn học cùng bọn tớ 😅😅',
+    btnReply: 'Chọn vui rồi thì giải thích nốt lí do đi nè <3',
+    reply: 'Vì đó là cái duyên để tạo nên tập thể C3 đấy <33333333',
     mess: 'biet roi nek 😘😘',
-    messDesc: 'hom sau di hoc e.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Đợi đi học được thì chúng mình nói chuyện nhé .',
+    btnAccept: 'Cần thiết thì nhắn vào đây nè <3',
     messLink: 'https://www.instagram.com/c3k49geographie/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
